f3a000f409bf9a582bfa5d08e325cf56402138a0
Ajstudying <rkddowls1@gmail.com>
2025-02-07 14:44:21 +0900

    footer 문구 변경

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="w-full bg-[#fff] text-xs text-center">
       <div className="h-10 leading-10">
-        &copy; 2025. your_id All rights reserved.
+        &copy; 2025. Ajstudying All rights reserved.
       </div>
     </footer>
   );
